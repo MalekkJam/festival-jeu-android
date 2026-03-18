7e903d77dc200180b587315277b5ad4811066977
MalekkJam <malek.jamel@etu.umontpellier.fr>
2026-03-18 12:37:49 +0100

    added the login logout mechanism and tested it

---
+++ app/src/main/java/com/example/festivaljeumobile/domain/model/User.kt
@@ -0,0 +1,17 @@
+package com.example.festivaljeumobile.domain.model
+
+data class User(
+    val id: Long? = null,
+    val login: String,
+    val role: UserRole,
+    val password: String? = null,
+    val prenom: String? = null,
+    val nom: String? = null
+)
+
+enum class UserRole {
+    Benevole,
+    Organisateur,
+    SuperOrganisateur,
+    Admin
+}
